25e123966343e51e58b68529d76fae579d84f4f3
unknown <zavrip78>
2020-09-11 14:51:41 +1200

    Added Checkboxes to table

--- scripts/script.js
+++ scripts/script.js
@@ -2,7 +2,22 @@ function getData() {
 
     fetch('scripts/data.json')
     .then(resp =>resp.json())
-    .then(data => console.log(data))
+    .then(data => {
+
+        let htmlStuff = data.map(d => {
+            return `
+            <tr>
+            <td>
+            <input type="checkbox" value=${d.completed}" ${d.completed ? "checked" : null}>
+            </td>
+            <td>${d.title}</td>
+            <td>${d.completed}</td>
+            </tr>`
+        }).join(" ")
+
+        // console.log(htmlStuff)
+        document.querySelector('tbody').innerHTML = htmlStuff
+    })
 }
 
 getData()
